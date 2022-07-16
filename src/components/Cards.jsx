06265fda68746838252df32c96028c61e2797216
AustinD14 <austindimalanta14@gmail.com>
2022-07-16 15:04:00 -0700

    adding matches and winrates

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,19 +11,52 @@ function Cards(props) {
   const NUM_OF_HEROES_TO_DISPLAY = 10;
   const heroList = [];
 
+  const calculateWinRate = (wins, total) => {
+    return (wins / total) * 100;
+  };
+
+  console.log({
+    name: heroes[props.props.mostPlayedHeroes[0].hero_id].localized_name,
+    games: props.props.mostPlayedHeroes[0].games,
+    winrate:
+      Math.floor(
+        calculateWinRate(
+          props.props.mostPlayedHeroes[0].win,
+          props.props.mostPlayedHeroes[0].games
+        )
+      ) + "%",
+  });
+
   for (let index = 0; index < NUM_OF_HEROES_TO_DISPLAY; index++) {
     heroList.push(
-      heroes[props.props.mostPlayedHeroes[index].hero_id].localized_name
+      // heroes[props.props.mostPlayedHeroes[index].hero_id].localized_name
+      {
+        name: heroes[props.props.mostPlayedHeroes[index].hero_id]
+          .localized_name,
+        games: props.props.mostPlayedHeroes[index].games,
+        winrate:
+          Math.floor(
+            calculateWinRate(
+              props.props.mostPlayedHeroes[index].win,
+              props.props.mostPlayedHeroes[index].games
+            )
+          ) + "%",
+      }
     );
   }
 
   const listHeroes = heroList.map((heroList) => (
     <ListGroup.Item className="hero_list" key={heroList}>
-      {heroList}
+      <div className="ms-2 me-auto">
+        <div className="fw-bold">
+          <strong>{heroList.name}</strong>
+        </div>
+        {heroList.games} Matches with {heroList.winrate} winrate
+      </div>
     </ListGroup.Item>
   ));
 
-  console.log(heroList);
+  console.log(props.props.mostPlayedHeroes[0].hero_id);
   return (
     <Card
       bg="primary"
@@ -41,9 +74,7 @@ function Cards(props) {
             </Card.Subtitle>
           </div>
         </div>
-        <ListGroup>
-          {listHeroes}
-        </ListGroup>
+        <ListGroup>{listHeroes}</ListGroup>
       </Card.Body>
     </Card>
   );
